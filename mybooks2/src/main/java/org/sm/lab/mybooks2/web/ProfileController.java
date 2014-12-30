5641822de057c7892e52da5e00cb367197352e22
Stanislav Markov <mstane@gmail.com>
2014-12-30 12:14:22 +0100

    Profile and Admin parts.

--- mybooks2/src/main/java/org/sm/lab/mybooks2/web/ProfileController.java
+++ mybooks2/src/main/java/org/sm/lab/mybooks2/web/ProfileController.java
@@ -20,87 +20,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
-@RequestMapping("/readers")
+@RequestMapping("/profile")
 @Controller
-public class ReaderController {
+public class ProfileController {
 
 	@Autowired
     ReaderService readerService;
 
 	@Autowired
     BookService bookService;
-
-	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Reader reader, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, reader);
-            return "readers/create";
-        }
-        uiModel.asMap().clear();
-        readerService.saveReader(reader);
-        return "redirect:/readers/" + encodeUrlPathSegment(reader.getId().toString(), httpServletRequest);
-    }
-
-	@RequestMapping(params = "form", produces = "text/html")
-    public String createForm(Model uiModel) {
-        populateEditForm(uiModel, new Reader());
-        return "readers/create";
-    }
 	
 	@RequestMapping(params = "register", method = RequestMethod.POST, produces = "text/html")
     public String register(@Valid Reader reader, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, reader);
-            return "readers/register";
+            return "profile/register";
         }
         uiModel.asMap().clear();
-        reader.setSystemRole(SystemRole.Common);
+        reader.setSystemRole(SystemRole.Admin);
         readerService.saveReader(reader);
-        return "redirect:/readers/" + encodeUrlPathSegment(reader.getId().toString(), httpServletRequest);
+        return "redirect:/profile/" + encodeUrlPathSegment(reader.getId().toString(), httpServletRequest);
     }
 
 	@RequestMapping(params = "register", produces = "text/html")
     public String registerForm(Model uiModel) {
         populateEditForm(uiModel, new Reader());
-        return "readers/register";
+        return "profile/register";
     }		
 
 	@RequestMapping(value = "/{id}", produces = "text/html")
     public String show(@PathVariable("id") String id, Model uiModel) {
         uiModel.addAttribute("reader", readerService.findReader(id));
         uiModel.addAttribute("itemId", id);
-        return "readers/show";
-    }
-
-	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
-        if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
-            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("readers", readerService.findReaderEntries(firstResult, sizeNo));
-            float nrOfPages = (float) readerService.countAllReaders() / sizeNo;
-            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
-            uiModel.addAttribute("readers", readerService.findAllReaders());
-        }
-        return "readers/list";
+        return "profile/show";
     }
 
 	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
     public String update(@Valid Reader reader, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, reader);
-            return "readers/update";
+            return "profile/update";
         }
         uiModel.asMap().clear();
         readerService.updateReader(reader);
-        return "redirect:/readers/" + encodeUrlPathSegment(reader.getId().toString(), httpServletRequest);
+        return "redirect:/profile/" + encodeUrlPathSegment(reader.getId().toString(), httpServletRequest);
     }
 
 	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
     public String updateForm(@PathVariable("id") String id, Model uiModel) {
         populateEditForm(uiModel, readerService.findReader(id));
-        return "readers/update";
+        return "profile/update";
     }
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
@@ -108,9 +77,7 @@ public class ReaderController {
         Reader reader = readerService.findReader(id);
         readerService.deleteReader(reader);
         uiModel.asMap().clear();
-        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
-        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
-        return "redirect:/readers";
+        return "redirect:/";
     }
 
 	void populateEditForm(Model uiModel, Reader reader) {
