d9706e3db5b980a0dca0e12c532d43ae68d3927b
Stanislav Markov <mstane@gmail.com>
2015-06-08 15:31:52 +0200

    AngularJS Datepicker. Fixed issue not showing glyphicons with wro4j - copied fonts direcotry from bootstrap webjar to project's static directory.

--- mybooks3/src/main/resources/static/js/app.js
+++ mybooks3/src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('hello', [ 'ngRoute', 'ngResource' ]);
+var app = angular.module('hello', [ 'ngRoute', 'ngResource', 'ui.bootstrap' ]);
 
 
 app.config(function($routeProvider, $httpProvider) {
@@ -254,7 +254,31 @@ app.controller("BookController", function ($scope, BookFactory, $location) {
 	        });
 		}    	
     };
+    
+    
+    
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
 
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+
+    $scope.format = 'dd-MMMM-yyyy';
+
+    
+    
+    
     init();
 });
 
