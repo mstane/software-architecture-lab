098e812841965cf83780adce87a53c047bec4e47
Stanislav Markov <mstane@gmail.com>
2016-12-15 11:57:54 +0000

    Removed client files: js, html and css. They have been moved in https://github.com/mstane/mybooks-client-angularjs repository.

--- src/main/resources/META-INF/resources/static/app/js/profiles/profileFactories.js
+++
@@ -1,8 +0,0 @@
-var profileFactories = angular.module('profileFactories', ['ngResource']);
-
-profileFactories.factory("ProfileFactory", function ($resource) {
-    return $resource(URLS.profilesRest, {id: "@id"}, {
-        update: { method: 'PUT' }
-    });
-});
-
--- src/test/resources/static/js/e2e/scenarios.js
+++
@@ -1,1 +0,0 @@
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
